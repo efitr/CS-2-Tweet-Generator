634708fcaca0ffc05b6eb553ded18030198099c2
Egon Fiedler <efiedler93@gmail.com>
2017-11-08 11:26:57 -0800

    update heroku

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def hello_world():
     return "Fuck Egon!!!!!"
 
 if __name__ == "__main__":
-    port = int(os.environs.get('PORT', 9001))
+    port = int(os.environ.get('PORT', 9001))
     app.run(host='0.0.0.0' , port=port)
