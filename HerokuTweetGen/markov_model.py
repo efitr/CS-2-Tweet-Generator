db9d7efdfbb8cc32daf37fb1b1c2992881aae2e6
Egon Fiedler <efiedler93@gmail.com>
2017-12-21 22:54:06 -0800

    update

--- HerokuTweetGen/markov_model.py
+++ HerokuTweetGen/markov_model.py
@@ -24,75 +24,7 @@ def any_order_markov_model(order,source):
             markov_model[window] = Dictogram([source[i+order]])
     return markov_model
 
-def generate_random_start(model):
-    if 'END' in model:
-        end_word = 'END'
-        while end_word == 'END':
-            end_word = model['END'].return_weighted_random_word()
-        return end_word
-    return random.choice(list(model.keys()))
-    pass
-
-def get_start_token(markov):
-    first_word = random.choice(list(markov.keys()))
-    return first_word
-
-def generate_sentence(lenght, markov_model):
-    current_word = generate_random_start(markov_model)
-    sentence = [current_word]
-    for i in range(0, lenght):
-        current_dictogram = markov_model[current_word]
-        random_word = current_dictogram.return_weighted_random_word()
-        current_word = random_word
-        sentence.append(current_word)
-    sentence[0] = sentence[0].capitalize()
-    return ' '.join(sentence) + '.'
-    return sentence
-
-def generate_random_sentence_n(lenght, markov_model):
-    current_window = get_start_token(markov_model)
-    sentence = [current_window[0]]
-    tweet = ''
-
-    valid_tweet_flag = True
-    sentence_count = 0
-    while valid_tweet_flag:
-        current_dictogram = markov_model[current_window]
-        random_weighted_word = current_dictogram.return_weighted_random_word()
-
-        current_window_deque = deque(current_window)
-        current_window_deque.popleft()
-        current_window_deque.append(random_weighted_word)
-        current_window = tuple(current_window_deque)
-        sentence.append(current_window[0])
-
-        if current_window[1] == 'end' or current_window[1] == '[end]':
-            sentence_string = ' '.join(sentence)
-            sentence_string = re.sub('end', '. ', sentence_string, flags=re.IGNORECASE)
-            sentence_string = sentence_string.capitalize()
-            new_tweet_len = len(sentence_string) + len(tweet)
-
-            if sentence_count == 0 and new_tweet_len < lenght:
-                tweet += sentence_string
-                sentence_string = ' '.join(sentence)
-                sentence_count += 1
-                current_window = generate_random_start(markov_model)
-                sentence = [current_window[0]]
-            
-            elif sentence_count == 0 and new_tweet_len >= lenght:
-                current_window = generate_random_start(markov_model)
-                sentence = [current_window[0]]
-            
-            elif sentence_count > 0 and new_tweet_len < lenght:
-                tweet += sentence_string
-                sentence_string = ' '.join(sentence)
-                sentence_count += 1
-                current_window = generate_random_start(markov_model)
-                sentence = [current_window[0]]
-            
-            else:
-                return tweet
-
 if __name__ == '__main__':
-    clean_text_list = clean_file('MobyDick.txt')
-    print(clean_text_list)
+    clean_text_list = clean_file('The Karamazov Brothers.rtf')
+    #print(clean_text_list)
+    markov_chaining = any_order_markov_model(2,clean_text_list)
